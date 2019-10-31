686fb33ad3eebfd2330f10dfe5d36025cd710f2d
Cameron King <cameronking42@gmail.com>
2019-10-31 11:26:18 -0500

    Refactors sequence-checking logic into a Vue-independent class

---
+++ src/KeySequenceChecker.js
@@ -0,0 +1,105 @@
+import Mousetrap from 'mousetrap';
+
+const modifiers = [
+    {binding: 'ctrl', attr: 'ctrlKey', key: 'Control'},
+    {binding: 'alt', attr: 'altKey', key: 'Alt'},
+    {binding: 'shift', attr: 'shiftKey', key: 'Shift'},
+];
+
+const modAttrs = modifiers.map(m => m.attr);
+const modKeys = modifiers.map(m => m.key);
+const bindings = modifiers.map(m => m.binding);
+
+export default class KeySequenceChecker {
+    constructor(el, sequence='', { success, failure }) {
+        this.el = el;
+        this.el.addEventListener('keydown', (e) => this.handleKeydown(e));
+        this.mousetrap = new Mousetrap(el);
+        this.sequence = sequence;
+        this.handlers = { success, failure };
+        this.keydowns = [];
+
+        if (this.sequence) {
+            this.initialize();
+        }
+    }
+
+    setSequence(seq) {
+        this.mousetrap.unbind(this.sequence);
+        this.sequence = seq;
+        this.initialize();
+    }
+
+    initialize() {
+        this.keydowns = [];
+        this.mousetrap.bind(this.sequence, () => this.alertSuccess());
+    }
+
+    alertSuccess() {
+        this.emit('success');
+    }
+
+    alertFailure() {
+        this.emit('failure', this.stringifyKeydowns());
+    }
+
+    emit(event, payload) {
+        this.handlers[event](payload);
+    }
+
+    stringifyKeydowns() {
+        return this.keydowns
+            .filter(e => !modKeys.includes(e.key))
+            .map(e => this.stringifyKeydown(e))
+            .join(' ')
+    }
+
+    stringifyKeydown(event) {
+        const mods = this.modifiersInEvent(event).map(m => m.binding).join('+');
+        const key = event.code.replace('Key', '').toLowerCase();
+        return mods ? `${mods}+${key}` : key;
+    }
+
+    modifiersInEvent(event) {
+        return modifiers.filter(({ attr }) => event[attr]);
+    }
+
+    handleKeydown(event) {
+        this.keydowns.push(event);
+        // for some reason I cannot discover, pressing 'Alt' in an input in Chrome focuses away
+        if (event.key == "Alt") event.preventDefault();
+
+        const nextCombo = this.getNextKeyCombo(event);
+
+        modifiers.forEach(({ key, binding }) => {
+            if (event.key == key && !nextCombo.includes(binding)) {
+                this.alertFailure(event)
+            }
+        })
+        if (this.isWrong(nextCombo, event)) {
+            this.alertFailure();
+        }
+    }
+
+    getNextKeyCombo(event) {
+        let keypressIdx = this.keydowns.filter(e => !modKeys.includes(e.key)).length - 1;
+        if (keypressIdx == -1) keypressIdx = 0;
+        return this.sequence.split(' ')[keypressIdx];
+    }
+
+    isWrong(nextCombo, event) {
+        return !nextCombo.includes(event.key) ||
+            this.hasUnexpectedModifier(nextCombo, event) ||
+            this.isMissingModifier(nextCombo, event)
+    }
+
+    hasUnexpectedModifier(nextCombo, event) {
+        return bindings.some(binding =>
+            !nextCombo.includes(binding) && event[`${binding}Key`])
+    }
+
+    isMissingModifier(nextCombo, event) {
+        return bindings.some(binding =>
+            nextCombo.includes(binding) && !event[`${binding}Key`])
+    }
+}
--- src/components/KeyInput.vue
+++ src/components/KeyInput.vue
@@ -1,96 +1,24 @@
 <script>
-import Mosuetrap from 'mousetrap';
-
-const modifiers = [
-    {binding: 'ctrl', attr: 'ctrlKey', key: 'Control'},
-    {binding: 'alt', attr: 'altKey', key: 'Alt'},
-    {binding: 'shift', attr: 'shiftKey', key: 'Shift'},
-];
-
-const modAttrs = modifiers.map(m => m.attr);
-const modKeys = modifiers.map(m => m.key);
-const mods = modifiers.map(m => m.binding);
+import KeySequenceChecker from '../KeySequenceChecker';
 
 export default {
     props: ['expected'],
     data() {
         return {
-            mousetrap: null,
-            keydowns: [],
+            checker: null,
         }
     },
     mounted() {
-        this.mousetrap = new Mousetrap(this.$refs.input);
-        this.initialize();
+        this.checker = new KeySequenceChecker(this.$refs.input, this.expected, {
+            success: ()  => this.$emit('success'),
+            failure: (actual) => this.$emit('failure', actual)
+        });
     },
     watch: {
         expected(val, oldVal) {
-            this.mousetrap.unbind(oldVal);
-            this.initialize();
+            this.checker.setSequence(val);
         }
     },
-    methods: {
-        initialize() {
-            this.keydowns = [];
-            if (this.expected) {
-                this.mousetrap.bind(this.expected, this.alertSuccess);
-            }
-        },
-        alertSuccess(event) {
-            event.preventDefault();
-            this.$emit('success');
-        },
-        alertFailure() {
-            const actualKeypress = this.keydowns
-                .filter(e => !modKeys.includes(e.key))
-                .map(this.stringifyKeypress)
-                .join(' ')
-            this.$emit('failure', actualKeypress);
-        },
-        stringifyKeypress(event) {
-            const mods = this.modifiersInEvent(event).map(m => m.binding).join('+');
-            const key = event.code.replace('Key', '').toLowerCase();
-            return mods ? `${mods}+${key}` : key;
-        },
-        modifiersInEvent(event) {
-            return modifiers.filter(({ attr }) => event[attr]);
-        },
-        hasUnexpectedModifier(nextSequence, event) {
-            return mods.some(modifier =>
-                !nextSequence.includes(modifier) && event[`${modifier}Key`])
-        },
-        isMissingModifier(nextSequence, event) {
-            return mods.some(modifier =>
-                nextSequence.includes(modifier) && !event[`${modifier}Key`])
-        },
-        handleKeydown(event) {
-            this.keydowns.push(event);
-            // for some reason I cannot discover, pressing 'Alt' in an input in Chrome focuses away
-            if (event.key == "Alt") event.preventDefault();
-
-            let keypressIdx = this.keydowns.filter(e => !modKeys.includes(e.key)).length - 1;
-            if (keypressIdx >= this.expected.split(' ').length) {
-                this.alertFailure(event);
-            }
-            if (keypressIdx < 0) keypressIdx = 0;
-
-            const nextSequence = this.expected.split(' ')[keypressIdx]
-
-            modifiers.forEach(({ key, binding }) => {
-                if (event.key == key && !nextSequence.includes(binding)) {
-                    this.alertFailure(event)
-                }
-            })
-            if (this.isWrong(nextSequence, event)) {
-                this.alertFailure();
-            }
-        },
-        isWrong(nextSequence, event) {
-            return !nextSequence.includes(event.key) ||
-                this.hasUnexpectedModifier(nextSequence, event) ||
-                this.isMissingModifier(nextSequence, event)
-        },
-    }
 }
 </script>
 
@@ -102,6 +30,5 @@ export default {
         ref="input"
         data-cy="key-input"
         @input="(e) => e.target.value = ''"
-        @keydown="handleKeydown"
     />
 </template>
